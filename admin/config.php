14e285b3aa7b41dc12314a54adf2732a0dbaa401
Justin Sarnik <Sarnikdev@gmail.com>
2016-03-25 15:03:47 -0700

    change phpt config back to local db server

--- admin/config.php
+++ admin/config.php
@@ -1,15 +1,8 @@
 <?php
 
-//define('db_host', 'localhost');
-//define('db_name', 'LightRegions');
-//define('db_user', 'LR_Admin_Db');
-//define('db_pass', 'WweV$2Z');
-
-
-define('db_host', '50.62.209.7:3306');
+define('db_host', 'localhost');
 define('db_name', 'LightRegions');
-define('db_user', 'LR_Admin');
+define('db_user', 'LR_Admin_Db');
 define('db_pass', 'WweV$2Z');
-
 
 ?>
